9b1e80b3fa8623247105f5c963f3e842f5455d5d
Michal <terox@wp.pl>
2023-01-17 20:47:42 +0100

    6.1 Added kodilla-testing module and extra exercise

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,5 +3,8 @@ package com.kodilla.testing;
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        VowelExtract vowel = new VowelExtract();
+        String extracted = vowel.extract("Test of the Extraction");
+        System.out.println(extracted);
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/VowelExtract.java
@@ -0,0 +1,34 @@
+package com.kodilla.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VowelExtract {
+  private List<Character> vovelList;
+
+    public VowelExtract() {
+        vovelList = new ArrayList<>();
+        vovelList.add('i');
+        vovelList.add('I');
+        vovelList.add('y');
+        vovelList.add('Y');
+        vovelList.add('e');
+        vovelList.add('E');
+        vovelList.add('a');
+        vovelList.add('A');
+        vovelList.add('o');
+        vovelList.add('O');
+        vovelList.add('u');
+        vovelList.add('U');
+
+    }
+    public String extract(String text) {
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            if(vovelList.contains(text.charAt(i))) {
+                result += text.charAt(i);
+            }
+        }
+            return result;
+    }
+}
